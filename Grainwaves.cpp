ce7640799ec5af07151bf349422645305224c704
Leon Turpin <turpinleon@gmail.com>
2022-08-17 18:42:49 +0800

    Pitch shifting

--- Grainwaves.cpp
+++ Grainwaves.cpp
@@ -9,7 +9,8 @@ using namespace daisysp;
 
 DaisyPatchSM patch;
 CpuLoadMeter cpuLoadMeter;
-Switch       button;
+Switch       record_button;
+Switch       shift_button;
 
 uint8_t DMA_BUFFER_MEM_SECTION oled_buffer[SSD1327_REQUIRED_DMA_BUFFER_SIZE];
 Daisy_SSD1327 oled;
@@ -27,6 +28,7 @@ struct Grain {
     size_t start_offset = 0;
     size_t step = 0;
     float pan = 0; // 0 is left, 1 is right
+    float playback_speed = 0;
 };
 
 // Loopers and the buffers they'll use
@@ -53,7 +55,7 @@ float fwrap(float x, float min, float max) {
     return (x >= 0 ? min : max) + fmodf(x, max - min);
 }
 
-float wrap(int x, int min, int max) {
+int wrap(int x, int min, int max) {
     if (max == min) return min;
     if (min > max) return wrap(x, max, min);
 
@@ -64,6 +66,15 @@ float randF(float min, float max) {
     return min + rand() * kRandFrac * (max - min);
 }
 
+float mapToRange(float fraction, float min, float max) {
+    return min + fraction * (max - min);
+}
+
+float modf(float x) {
+    static float junk;
+    return modf(x, &junk);
+}
+
 void AudioCallback(
     AudioHandle::InputBuffer  in,
     AudioHandle::OutputBuffer out,
@@ -72,18 +83,21 @@ void AudioCallback(
     cpuLoadMeter.OnBlockStart();
 
     patch.ProcessAllControls();
-    button.Debounce();
+    record_button.Debounce();
+    shift_button.Debounce();
 
-    spawn_rate = (1 - patch.GetAdcValue(CV_1)) * 48000;
-    grain_length = min_grain_size + patch.GetAdcValue(CV_2) * (max_grain_size - min_grain_size);
-    float scan_speed = (patch.GetAdcValue(CV_3) -0.5) * 4;
+    spawn_rate = mapToRange(1 - patch.GetAdcValue(CV_1), min_grain_size, max_grain_size);
+    grain_length = mapToRange(patch.GetAdcValue(CV_2), min_grain_size, max_grain_size);
+    float scan_speed = mapToRange(patch.GetAdcValue(CV_3), -2, 2);
     float grain_spread = patch.GetAdcValue(CV_4);
     pan_spread = patch.GetAdcValue(CV_5);
     float spawn_rate_spread = patch.GetAdcValue(CV_6);
     float modified_spawn_rate = spawn_rate * (1 + next_spawn_offset * spawn_rate_spread);
-    
+    float pitch_shift_in_semitones = mapToRange(patch.GetAdcValue(CV_7), -12 * 5, 12 * 5); // volt per octave
+    float pitch_shift_spread = patch.GetAdcValue(CV_8);
+
     // Toggle the record state on button press
-    if(button.RisingEdge())
+    if(record_button.RisingEdge())
     {
         if (!is_recording) {
             is_recording = true;
@@ -94,7 +108,7 @@ void AudioCallback(
     }
 
     // Set the led to 5V if the looper is recording
-    patch.SetLed(is_recording); 
+    patch.SetLed(is_recording || shift_button.Pressed()); 
 
     // TODO: Fade in/out the ends of the track to prevent pops
     // TODO: Windowing to get rid of pops and squeaks
@@ -125,10 +139,16 @@ void AudioCallback(
                 size_t next_grain_to_spawn = available_grains.PopBack();
 
                 last_spawn_time = samples_seen;
+
                 grains[next_grain_to_spawn].length = grain_length;
                 grains[next_grain_to_spawn].start_offset = wrap(grain_start_offset + grain_spread * randF(-0.5f, 0.5f) * recording_length, 0, recording_length);
                 grains[next_grain_to_spawn].step = 0;
                 grains[next_grain_to_spawn].pan = 0.5f + randF(-0.5f, 0.5f) * pan_spread;
+
+                float pitch_shift_offset_in_semitones = randF(-2.f, 2.f) * pitch_shift_spread;
+                float pitch_shift_in_octaves = (pitch_shift_in_semitones + pitch_shift_offset_in_semitones) / 12.f;
+                float playback_speed = pow(2, pitch_shift_in_octaves);
+                grains[next_grain_to_spawn].playback_speed = playback_speed;
 
                 next_spawn_offset = randF(-1.f, 1.f);
             }
@@ -139,19 +159,27 @@ void AudioCallback(
 
             for (int j = 0; j < max_grain_count; j++) {
                 if (grains[j].step <= grains[j].length) {
-                    size_t buffer_index = wrap(grains[j].start_offset + grains[j].step, 0, recording_length);
+                    size_t buffer_index = wrap(grains[j].start_offset + grains[j].step * grains[j].playback_speed, 0, recording_length);
+
+                    // playback_speed is a float so we need to interpolate between samples
+                    float sample = buffer[buffer_index];
+                    float nextSample = buffer[wrap(buffer_index + 1, 0, recording_length)];
+
+                    float decimal_portion = modf(grains[j].step * grains[j].playback_speed);
+                    float interpolated_sample = sample * (1 - decimal_portion) + nextSample * decimal_portion;
 
                     // hacky bad envelope
+                    float envelope_mult = std::min((grains[j].length - grains[j].step), grains[j].step);
                     // TODO: Get rid of this 0.75f and balance the output properly
-                    float signal = buffer[buffer_index] * std::min((grains[j].length - grains[j].step), grains[j].step) / grains[j].length * 0.75f;
+                    float signal = interpolated_sample * envelope_mult / grains[j].length * 0.75f;
                     wetL += (1.f - grains[j].pan) * signal;
                     wetR += grains[j].pan * signal;
 
-                    if (grains[j].step == grains[j].length) {
+                    grains[j].step++;
+
+                    if (grains[j].step > grains[j].length) {
                         available_grains.PushBack(j);
                     }
-                    
-                    grains[j].step++;
                 }
             }
 
@@ -171,7 +199,8 @@ uint32_t last_debug_print_millis = 0;
 int main(void)
 {
     patch.Init();
-    button.Init(patch.B7);
+    record_button.Init(patch.B7);
+    shift_button.Init(patch.B8);
     patch.StartLog();
 
     spi.Init(
@@ -199,17 +228,20 @@ int main(void)
         if (System::GetNow() - last_render_millis > 8 && !oled.isRendering()) {
             last_render_millis = System::GetNow();
 
-            oled.clear(SSD1327_BLACK);
+            if (recording_length == 0) {
+                oled.clear(0x2);
+            } else {
+                oled.clear(SSD1327_BLACK);
+            }
 
             // Grain start offset
             float grain_start_offset_y = grain_start_offset / (float)recording_length * oled.height;
-            float grain_start_offset_y_2 = grain_start_offset_y - (uint8_t)grain_start_offset_y;
-            float grain_start_offset_y_1 = 1 - grain_start_offset_y_2;
+            float grain_start_offset_y_decimal_part = modf(grain_start_offset_y);
             uint8_t x_margin = (oled.width - (pan_spread * oled.width)) / 2;
 
             for (uint8_t x = x_margin; x < oled.width - x_margin; x++) {
-                oled.setPixel(x, grain_start_offset_y, 0x2 + 0x4 * grain_start_offset_y_1);
-                oled.setPixel(x, wrap(grain_start_offset_y + 1, 0, oled.height), 0x2 + 0x4 * grain_start_offset_y_2);
+                oled.setPixel(x, grain_start_offset_y, mapToRange(1 - grain_start_offset_y_decimal_part, 0x2, 0x6));
+                oled.setPixel(x, wrap(grain_start_offset_y + 1, 0, oled.height), mapToRange(grain_start_offset_y_decimal_part, 0x2, 0x6));
             }
 
             // Grains
@@ -218,7 +250,7 @@ int main(void)
 
                 if (grain.step <= grain.length) {
                     uint8_t x = grain.pan * oled.width;
-                    uint32_t currentOffset = wrap(grain.start_offset + grain.step, 0, recording_length);
+                    uint32_t currentOffset = wrap(grain.start_offset + grain.step * grains[j].playback_speed, 0, recording_length);
                     float y = (currentOffset / (float)recording_length) * oled.height;
 
                     float amplitude = std::min((grains[j].length - grains[j].step), grains[j].step) / (float)grains[j].length;
@@ -226,6 +258,7 @@ int main(void)
                     oled.setPixel(x, y, 0xF * amplitude);
                 }
             }
+
             oled.display();
         }
 
@@ -234,6 +267,7 @@ int main(void)
 
             // Note, this ignores any work done in this loop, eg running the OLED
             patch.PrintLine("cpu Max:" FLT_FMT3 "\tAvg:" FLT_FMT3, FLT_VAR3(cpuLoadMeter.GetMaxCpuLoad()), FLT_VAR3(cpuLoadMeter.GetAvgCpuLoad()));
+            // patch.PrintLine(FLT_FMT3, FLT_VAR3(round(mapToRange(patch.GetAdcValue(CV_7), -12, 12)) / 12));
             // patch.PrintLine("grain_start_offset: " FLT_FMT3, FLT_VAR3(grain_start_offset));
         }
     }
